82dd9fc67ad88328f84df1ee226a4c21f92c8acc
Justus <justus@ju5tu5.nl>
2011-10-18 21:42:23 +0200

    Add some style

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<title>Serverside Scripting Voorbeeldcode</title>
+	<link href="/style/style_normal.css" rel="Stylesheet" type="text/css" media="all" />
+</head>
+<body>
+
+	<h1><span>Serverside Scripting</span></h1>
+
+</body>
+</html>
